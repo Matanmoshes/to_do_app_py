c576ad5f68decc0304e84902aadd1a71bb993f47
Matan Moshe <matanmoshe@Matans-MacBook-Pro.local>
2023-09-02 21:26:50 +0300

    changed in the show case remove the \n

--- main.py
+++ main.py
@@ -22,8 +22,9 @@ while True:
             file = open('todos.txt', 'r')
             todos = file.readlines()
             file.close()
-
+            
             for index, item in enumerate(todos):
+                item = item.strip('\n')
                 row = f"{index + 1}-{item.title()}"
                 print(row)              
         case 'edit':
