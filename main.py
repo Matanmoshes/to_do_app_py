c77dccbdc41ee06842a96b31f18bdbe46bc82135
Matan Moshe <matanmoshe@Matans-MacBook-Pro.local>
2023-10-03 21:34:11 +0300

    latest version changed all the cases to use 'with'

--- main.py
+++ main.py
@@ -8,33 +8,51 @@ while True:
     match user_action:
         case 'add':
             todo = input("Enter a todo: ") + "\n"
-            
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
-            
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
             todos.append(todo)
 
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'show':
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
             
             for index, item in enumerate(todos):
                 item = item.strip('\n')
                 row = f"{index + 1}-{item.title()}"
-                print(row)              
+                print(row)  
+
         case 'edit':
             number = int(input("Number of the todo edit: "))
             number = number - 1 
+
+            with open('todos.txt', 'r') as file:
+                file.readlines()
+
             new_todo = input("Enter new todo: ")
-            todos[number] = new_todo
+            todos[number] = new_todo + '\n'
+            
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+            
         case 'complete':
             number = int(input("Number of the todo to complete: "))
-            todos.pop(number - 1)
+            
+            with open('todos.txt', 'r') as file:
+                file.readlines()
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list."
+            print(message)
         case 'exit':
             break
         case _:
