2f4cb8911963ce69cf0f85f0950d00df69fae48c
Matan Moshe <matanmoshe@Matans-MacBook-Pro.local>
2023-10-14 14:36:24 +0300

    buliding gui change the sturcher

--- functions.py
+++ functions.py
@@ -1,13 +1,18 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """ Read a text file and return the list of to-do items."""
 
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_local, filepath="todos.txt"):
+def write_todos(todos_local, filepath=FILEPATH):
     """ Write the to-do items list in the text file. """
     
     with open(filepath, 'w') as file:
             file.writelines(todos_local)
 
+if __name__ == "__main__":
+     print("Hello")
+     print(get_todos())
