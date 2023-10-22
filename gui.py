b4a9833c86d270dba9be4ec1a65c11ab6693d3ca
Matan Moshe <matanmoshe@Matans-MacBook-Pro.local>
2023-10-22 19:02:00 +0300

    final code with comment

--- gui.py
+++ gui.py
@@ -1,19 +1,28 @@
-import functions
-import PySimpleGUI as sg
-import time
+import functions  # Importing the custom 'functions' module which likely contains todo-related functions.
+import PySimpleGUI as sg  # Importing the PySimpleGUI library for the graphical user interface.
+import time  # Importing the time module to work with time-related functions.
+import os  # Importing the os module to interact with the operating system.
 
+# Check if the todos.txt file exists, if not, create an empty file.
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", 'w') as file:
+        pass
+
+# Setting the theme for the GUI window.
 sg.theme("Black ")
 
-clock = sg.Text('', key="clock")
-label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+# GUI Element Definitions:
+clock = sg.Text('', key="clock")  # A text element to display the current time.
+label = sg.Text("Type in a to-do")  # A label for the input box.
+input_box = sg.InputText(tooltip="Enter todo", key="todo")  # An input box for the user to enter todos.
+add_button = sg.Button("Add")  # A button that triggers the addition of a new todo.
 list_box = sg.Listbox(values=functions.get_todos(), key="todos", 
-                      enable_events=True, size=[45, 10])
-edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
-exit_button = sg.Button("Exit")
+                      enable_events=True, size=[45, 10])  # A listbox to display current todos.
+edit_button = sg.Button("Edit")  # A button that allows the user to edit a selected todo.
+complete_button = sg.Button("Complete")  # A button that marks a todo as complete.
+exit_button = sg.Button("Exit")  # A button to exit the application.
 
+# Window layout definition.
 window = sg.Window('My To-Do App',
                     layout=[[clock],
                             [label], 
@@ -22,45 +31,44 @@ window = sg.Window('My To-Do App',
                             [exit_button]], 
                     font=('Helvetice', 20))
 
+# Event Loop
 while True:
-    event, values = window.read(timeout=200)
-    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S")) 
-    match event:
-        case "Add":
-            todos = functions.get_todos()
-            new_todo = values['todo'] + '\n'
-            todos.append(new_todo)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-        case "Edit":
+    event, values = window.read(timeout=200)  # Read events and values from the window.
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))  # Update the clock element with the current time.
+    match event:  # Using match-case to handle events.
+        case "Add":  # When the 'Add' button is clicked.
+            todos = functions.get_todos()  # Retrieve current todos.
+            new_todo = values['todo'] + '\n'  # Get the new todo from the input box.
+            todos.append(new_todo)  # Add the new todo to the list of todos.
+            functions.write_todos(todos)  # Save the updated todos list.
+            window['todos'].update(values=todos)  # Update the listbox with the new list of todos.
+        case "Edit":  # When the 'Edit' button is clicked.
             try:
-                todo_to_edit = values['todos'][0]
-                new_todo = values['todo']
+                todo_to_edit = values['todos'][0]  # Get the selected todo to edit.
+                new_todo = values['todo']  # Get the updated todo text from the input box.
                 
-                todos = functions.get_todos()
-                index = todos.index(todo_to_edit)
-                todos[index] = new_todo
-                functions.write_todos(todos)
-                window['todos'].update(values=todos)
-            except IndexError:
-                 sg.popup("please select an item first.", font=("Helvetice", 20))
-        case "Complete":
+                todos = functions.get_todos()  # Retrieve current todos.
+                index = todos.index(todo_to_edit)  # Find the index of the todo to edit.
+                todos[index] = new_todo  # Replace the old todo with the updated text.
+                functions.write_todos(todos)  # Save the updated todos list.
+                window['todos'].update(values=todos)  # Update the listbox with the new list of todos.
+            except IndexError:  # Handle the case where no todo is selected.
+                 sg.popup("please select an item first.", font=("Helvetice", 20))  # Show a popup for user notification.
+        case "Complete":  # When the 'Complete' button is clicked.
             try:
-                todo_to_complete = values['todos'][0]
-                todos = functions.get_todos()
-                todos.remove()
-                functions.write_todos(todos)
-                window['todos'].update(values=todos)
-                window['todo'].update(values='')
-            except IndexError:
-                sg.popup("please select an item first.", font=("Helvetice", 20))
-        case "Exit":
-            break
-        case "todos":
-            window['todo'].update(value=values['todos'][0 ])
+                todo_to_complete = values['todos'][0]  # Get the selected todo to mark as complete.
+                todos = functions.get_todos()  # Retrieve current todos.
+                todos.remove(todo_to_complete)  # Remove the completed todo from the list.
+                functions.write_todos(todos)  # Save the updated todos list.
+                window['todos'].update(values=todos)  # Update the listbox with the new list of todos.
+                window['todo'].update(values='')  # Clear the input box.
+            except IndexError:  # Handle the case where no todo is selected.
+                sg.popup("please select an item first.", font=("Helvetice", 20))  # Show a popup for user notification.
+        case "Exit":  # When the 'Exit' button is clicked.
+            break  # Exit the event loop.
+        case "todos":  # When an item in the listbox is selected.
+            window['todo'].update(value=values['todos'][0])  # Update the input box with the text of the selected todo.
+        case sg.WIN_CLOSED:  # When the window is closed.
+            break  # Exit the event loop.
 
-        case sg.WIN_CLOSED:
-            break
-
-
-window.close()
+window.close()  # Close the window after exiting the event loop.
--- web.py
+++ web.py
@@ -1,31 +1,25 @@
-import streamlit as st
-import functions
+import streamlit as st                # Importing the streamlit library, essential for the web app interface.
+import functions                      # Importing functions from the local functions script.
 
-todos = functions.get_todos()
+todos = functions.get_todos()         # Fetching the current list of todos from the file.
 
-def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
-    functions.write_todos(todos)
+def add_todo():                       # Function to handle the addition of a new todo item.
+    todo = st.session_state["new_todo"] + "\n"  # Retrieving the new todo item from the session state.
+    todos.append(todo)                # Adding the new todo item to the list of todos.
+    functions.write_todos(todos)      # Writing the updated list of todos back to the file.
 
+st.title("My Todo App")               # Setting the title of the web page.
+st.subheader("This is my todo app.")  # Adding a subheader to the web page.
+st.write("Thus app is to increase your productivity.")  # Providing a description or additional information.
 
-st.title("My Todo App")
-st.subheader("This is my todo app.")
-st.write("Thus app is to increase your productivity.")
-
-
-for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=f"todo {index}")
-    if checkbox:
-        todos.pop(index)
-        functions.write_todos(todos)
+for index, todo in enumerate(todos):  # Looping through each todo item in the list.
+    checkbox = st.checkbox(todo, key=f"todo {index}")  # Creating a checkbox for each todo item.
+    if checkbox:                      # Checking if the checkbox is selected/checked.
+        todos.pop(index)              # If checked, the todo item is removed from the list.
+        functions.write_todos(todos)  # The updated list, sans the completed todo, is written back to the file.
         #del st.session_state[todo]
-        st.experimental_rerun()
-#for index, todo in enumerate(todos):
-#    st.checkbox(todo, key=f"todo_{index}") 
+        st.experimental_rerun()       # Rerunning the app to reflect the changes immediately.
 
-
-st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key='new_todo')
-
+st.text_input(label="", placeholder="Add new todo...",  # Creating a text input field for adding new todos.
+              on_change=add_todo, key='new_todo')       # Specifying the function to call and the session state key.
 
